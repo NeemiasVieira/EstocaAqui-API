250a7cadd36db7db8c08548625c71ba400d4e802
Gabriel <gabriel.leandrods@gmail.com>
2023-11-29 17:01:26 -0300

    Rota update fornecedores

--- src/modules/fornecedores/use-cases/update-fornecedor/update-fornecedor.controller.ts
+++ src/modules/fornecedores/use-cases/update-fornecedor/update-fornecedor.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Patch } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UpdateFornecedorService } from './update-fornecedor.service';
+import { UpdateFornecedorDto } from './update-fornecedor.dto';
+import { Fornecedor } from '../../fornecedor.model';
 
 @Controller('update-fornecedor')
-export class UpdateFornecedorController {}
+@ApiTags("Fornecedor")
+export class UpdateFornecedorController {
+    constructor(private readonly appservice : UpdateFornecedorService){}
+
+    @Patch("/:id")
+    @ApiOperation({summary: "Atualiza o fornecedor"})
+    @ApiResponse({status: 200, description: "Atualização realizada com sucesso"})
+    @ApiResponse({status: 404, description: "Fornecedor não encontrado"})
+    async updateFornecedor(@Param("id") id: string, @Body() fornecedorAtualizado: UpdateFornecedorDto) : Promise<Fornecedor>{
+        return await this.appservice.updateFornecedor(id, fornecedorAtualizado)
+    }
+}
--- src/modules/fornecedores/use-cases/update-fornecedor/update-fornecedor.service.ts
+++ src/modules/fornecedores/use-cases/update-fornecedor/update-fornecedor.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { Fornecedor } from '../../fornecedor.model';
+import { UpdateFornecedorDto } from './update-fornecedor.dto';
 
 @Injectable()
-export class UpdateFornecedorService {}
+export class UpdateFornecedorService {
+
+    async updateFornecedor(id: string, fornecedorAtualizado : UpdateFornecedorDto) : Promise<Fornecedor>{
+        
+        const fornecedor = await Fornecedor.findOne({where: {id}});
+
+        if(!fornecedor) throw new HttpException("Fornecedor não encontrado", 404);
+
+        const { razao_social, nome_fantasia, cnpj} = fornecedorAtualizado
+
+        if(fornecedor) fornecedor.razao_social = razao_social;
+        if(fornecedor) fornecedor.nome_fantasia = nome_fantasia;
+        if(fornecedor) fornecedor.cnpj = cnpj;
+        await fornecedor.save();
+
+        return fornecedor;
+
+    }
+}
