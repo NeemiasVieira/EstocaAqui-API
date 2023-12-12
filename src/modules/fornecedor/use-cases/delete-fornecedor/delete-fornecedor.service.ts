fe002c7d107dff71021e86da83f353bafafca40b
Diogo Asenjo <diogoasenjo@outlook.com>
2023-12-12 11:56:29 -0300

    Ajustando rota para deletar fornecedor

--- src/modules/fornecedor/use-cases/delete-fornecedor/delete-fornecedor.service.ts
+++ src/modules/fornecedor/use-cases/delete-fornecedor/delete-fornecedor.service.ts
@@ -3,22 +3,22 @@ import { Fornecedor } from '../../fornecedor.model';
 
 @Injectable()
 export class DeleteFornecedorService {
+  private readonly logger = new Logger('DeleteFornecedorService');
 
-    private readonly logger = new Logger("DeleteFornecedorService");
-    
-    async DeleteFornecedor(id_fornecedor: String){
+  async DeleteFornecedor(id_fornecedor: number) {
+    this.logger.log(`Tentativa de exclusão do fornecedor ${id_fornecedor}`);
 
-        this.logger.log(`Tentativa de exclusão do fornecedor ${id_fornecedor}`)
+    const fornecedorExiste = await Fornecedor.findOne({
+      where: { id: id_fornecedor },
+    });
 
-            const fornecedorExiste = await Fornecedor.findOne({where: {id_fornecedor}});
-            
-            if(!fornecedorExiste){
-                this.logger.error("404 - Fornecedor não encontrado");
-                throw new HttpException ('Fornecedor não encontrado', 404);
-            } 
+    if (!fornecedorExiste) {
+      this.logger.error('404 - Fornecedor não encontrado');
+      throw new HttpException('Fornecedor não encontrado', 404);
+    }
 
-            this.logger.verbose(`Fornecedor ${id_fornecedor} excluído.`)
+    this.logger.verbose(`Fornecedor ${id_fornecedor} excluído.`);
 
-            await Fornecedor.destroy({where: {id_fornecedor}})
-        }
+    await Fornecedor.destroy({ where: { id: id_fornecedor } });
+  }
 }
