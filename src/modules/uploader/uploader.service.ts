9779286c20d44ae7bc9d09f2aa0e037ae2b555b4
Neemias Vieira <neemias.vieira@live.com>
2023-12-13 20:34:03 -0300

    Alteração no uploader

--- src/modules/uploader/uploader.service.ts
+++ src/modules/uploader/uploader.service.ts
@@ -1,5 +1,4 @@
 import { HttpException, Injectable, Logger } from '@nestjs/common';
-import { createReadStream, unlinkSync } from 'fs';
 import axios from 'axios';
 import * as FormData from 'form-data';
 import * as dotevn from 'dotenv';
@@ -10,35 +9,27 @@ dotevn.config();
 export class UploaderService {
     private readonly logger = new Logger("UploaderService");
 
-  async uploadImage(file: Express.Multer.File): Promise<any> {
+    async uploadImage(file: Express.Multer.File): Promise<any> {
+        this.logger.log("Upload de imagem");
 
-    this.logger.log("Upload de imagem");
+        const formData = new FormData();
+        formData.append('image', file.buffer, { filename: file.originalname });
 
-    const imageStream = createReadStream(file.path);
-    const formData = new FormData();
+        try {
+            const clientId = process.env.CLIENT_ID;
+            const response = await axios.post('https://api.imgur.com/3/image', formData, {
+                headers: {
+                    Authorization: `Client-ID ${clientId}`,
+                    ...formData.getHeaders(),
+                },
+            });
 
-    // Adicione o arquivo ao FormData com o nome 'image'
-    formData.append('image', imageStream, { filename: file.originalname });
+            if (response.data.data.link) this.logger.verbose("200 - Upload de imagem realizado");
 
-    try {
-      const clientId = process.env.CLIENT_ID; 
-      const response = await axios.post('https://api.imgur.com/3/image', formData, {
-        headers: {
-          Authorization: `Client-ID ${clientId}`,
-          ...formData.getHeaders(),
-        },
-      });
-
-      // Remova o arquivo temporário após o upload
-      unlinkSync(file.path);
-
-      if(response.data.data.link) this.logger.verbose("200 - Upload de imagem realizado");
-      
-      return response.data.data.link;
-    } catch (error) {
-        
-      this.logger.error("500 - Erro ao tentar realizar upload da imagem")
-      throw new HttpException('Falha ao fazer upload da imagem.', 500);
+            return response.data.data.link;
+        } catch (error) {
+            this.logger.error("500 - Erro ao tentar realizar upload da imagem");
+            throw new HttpException('Falha ao fazer upload da imagem.', 500);
+        }
     }
-  }
 }
