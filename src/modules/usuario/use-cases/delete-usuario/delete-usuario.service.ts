5c16e9484a8c0a2764578000c1a2e8893922837b
Diogo Asenjo <diogoasenjo@outlook.com>
2023-12-15 16:47:33 -0300

    Finalizando o delete usuário

--- src/modules/usuario/use-cases/delete-usuario/delete-usuario.service.ts
+++ src/modules/usuario/use-cases/delete-usuario/delete-usuario.service.ts
@@ -3,21 +3,42 @@ import { Usuario } from '../../usuario.model';
 
 @Injectable()
 export class DeleteUsuarioService {
-  private readonly logger = new Logger("DeleteUsuarioService");
+  private readonly logger = new Logger('DeleteUsuarioService');
 
-  async DeleteUsuario(id: number) {
-
+  async DeleteUsuario(id: number, id_grupo: number, id_usuario: number) {
     this.logger.log(`Tentativa de exclusão do usuário ${id}`);
 
-    const usuarioExiste = await Usuario.findOne({ where: { id } });
+    const usuarioExiste = await Usuario.findOne({ where: { id: id } });
 
-    if (!usuarioExiste){
-      this.logger.error("404 - Usuário não encontrado");
+    if (!usuarioExiste) {
+      this.logger.error('404 - Usuário não encontrado');
       throw new HttpException('Usuário não encontrado', 404);
-    } 
+    }
 
-    await Usuario.destroy({ where: { id } });
-    
+    if (usuarioExiste.id_grupo !== id_grupo) {
+      this.logger.error('401 - Usuário não autorizado');
+      throw new HttpException('Usuário não autorizado', 401);
+    }
+
+    if (usuarioExiste.id === id_usuario) {
+      this.logger.error('400 - Usuário não pode se deletar');
+      throw new HttpException('Usuário não pode se deletar', 400);
+    }
+
+    const todosUsuariosAdminDoGrupo = await Usuario.findAll({ where: { id_grupo: id_grupo, permissao: 'admin' } });
+
+    const idDosUsuariosAdmin = todosUsuariosAdminDoGrupo.map((usuarioAdmin) => {
+      return usuarioAdmin.dataValues.id;
+    });
+
+    if (!idDosUsuariosAdmin.includes(id_usuario)) {
+      this.logger.error('401 - Somente administadores podem deletar outros usuários');
+      throw new HttpException('Somente administadores podem deletar outros usuários', 401);
+    }
+
+    //Aqui dá problema para excluir usuário caso ele já tenha cadastrado algo. Trabalhar essa lógica.
+    //await Usuario.destroy({ where: { id: id } });
+
     this.logger.verbose(`200 - Usuário ${id} excluído.`);
   }
 }
