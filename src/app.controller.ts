167d9519f9cfc07a69765cb5516f887f4e91ec30
Diogo Asenjo <diogoasenjo@outlook.com>
2023-12-18 11:24:13 -0300

    ESLint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from './middlewares/auth-module/auth';
 
